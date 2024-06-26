f686d2b96fea12fa04788d761ef51bff30317140
Akshita Patel <akshita.patel@wazo.io>
2024-06-26 15:35:07 -0400

    Printing Monkey to wazo-calld logs

--- wazo_onboarding/plugins/plugin.py
+++ wazo_onboarding/plugins/plugin.py
@@ -6,4 +6,14 @@ logger = logging.getLogger(__name__)
 
 class Plugin:
     def load(self, dependencies):
-        logger.info("This is my new Plugin!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        # registering API endpoint
+        router = dependencies['router']
+        router.add_get('/onboarding', self.onboarding)
+        logger.info('Onboarding plugin loaded')
+
+    def unload(self):
+        logger.info('Onboarding Plugin unloaded')
+
+    def onboarding(self, request):
+        logger.info('Monkey')
+        return 'Onboarding successful'
