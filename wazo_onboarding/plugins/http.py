f97a53ce4ed1e8c272124c803fdc04828959be6f
Akshita Patel <akshita.patel@wazo.io>
2024-07-04 11:11:32 -0400

    Detect Channel name Adding ACL

--- wazo_onboarding/plugins/http.py
+++ wazo_onboarding/plugins/http.py
@@ -3,11 +3,14 @@ import logging
 from wazo_calld.http import AuthResource
 from wazo_amid.auth import required_acl
 
+from wazo_calld.ari_ import CoreARI as AriClient
+
 logger = logging.getLogger(__name__)
 
 
 class OnboardingResource(AuthResource):
-    def __init__(self, amid_client):
+    def __init__(self, ari, amid_client):
+        self.ari = ari
         self.amid_client = amid_client
 
     @required_acl("calld.onboarding.read")
@@ -15,15 +18,35 @@ class OnboardingResource(AuthResource):
         logger.info("Hello!!!!!!!")
         return "Hello", 200
 
-    def post(self, call_tech, call_id):
-        response = self.amid_client.action(
-            "Redirect",
-            {
-                "Channel": f"{call_tech}/{call_id}",
-                "Exten": "3000",
-                "Context": "xivo-extrafeatures",
-                "Priority": "1",
-            },
-        )
+    @required_acl("calld.onboarding.update")
+    def put(self, call_tech, call_id):
 
-        return response
+        channels = self.ari.channels.list()
+        logger.info("Channels!!!")
+        if (len(channels) == 0):
+            logger.info("No channels currently")
+        else:
+            logger.info("Current Channels : ")
+            for channel in channels:
+                logger.info("Channel: %s", channel)
+            # channel = self.ari.channels.get(channels[0])
+            # logger.info("Channel name: %s ", channel['name'])
+            response = self.amid_client.action(
+                "Redirect",
+                {
+                    "Channel": f"{call_tech}/{call_id}",
+                    "Exten": "3000",
+                    "Context": "xivo-extrafeatures",
+                    "Priority": "1",
+                },
+            )
+
+        return response, channels
+
+# not pass channel names and detect current calls witout passing it as arguments
+# use ari interface , we used ami , can use amid
+# reinstate required acl and make it work with alphas token
+
+# remove call_id autodetect form list of channels
+# play monkeys
+# hangup after few seconds
--- wazo_onboarding/plugins/plugin.py
+++ wazo_onboarding/plugins/plugin.py
@@ -2,6 +2,10 @@ import logging
 
 from wazo_amid_client import Client as AmidClient
 from .http import OnboardingResource
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from wazo_calld.ari_ import CoreARI as AriClient
 
 logger = logging.getLogger(__name__)
 
@@ -10,13 +14,14 @@ class Plugin:
     def load(self, dependencies):
         # registering API endpoint
         api = dependencies['api']
+        ari: AriClient = dependencies['ari']
         config = dependencies['config']
         token_changed_subscribe = dependencies['token_changed_subscribe']
-        
+
         amid_client = AmidClient(**config['amid'])
 
         token_changed_subscribe(amid_client.set_token)
 
         api.add_resource(
-            OnboardingResource, '/onboarding/<call_tech>/<call_id>', resource_class_args=[amid_client]
+            OnboardingResource, '/onboarding/<call_tech>/<call_id>', resource_class_args=[ari.client, amid_client]
         )
